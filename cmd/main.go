90cd35f3fd7e3e29f03daa91bf791b7b25a41214
shooooooma415 <shoma20040415@icloud.com>
2025-03-15 16:33:27 +0900

    feat: hello world

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	v1 "picolor-backend/server/v1"
+)
+
+func main() {
+	router := v1.NewRouter()
+
+	log.Println("Server is running on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
---
+++ server/v1/hello.go
@@ -0,0 +1,15 @@
+package v1
+
+import (
+	v1 "picolor-backend/presentation/v1"
+
+	"github.com/gorilla/mux"
+)
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello", v1.HelloHandler).Methods("GET")
+
+	return router
+}
