8c8b90e45793a371228df5945c941ec307fb8af8
shooooooma415 <shoma20040415@icloud.com>
2025-03-16 02:52:31 +0900

    fix: entityの変更

--- app/domain/post/post.go
+++ app/domain/post/post.go
@@ -3,15 +3,14 @@ package post
 import (
 	"picolor-backend/app/domain/auth"
 	"picolor-backend/app/domain/color"
-	"time"
 )
 
 type Image string
 
 type Post struct {
-	UserId   auth.UserID
-	ColorId  color.ColorID
-	Rank     int
-	Image    Image
-	PostedAt time.Time
+	UserId     auth.UserID
+	ColorId    color.ColorID
+	Rank       int
+	Image      Image
+	PostedTime string
 }
