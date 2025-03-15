cb711151accdf7efcc0c44ab7fb8cd86000f306b
shooooooma415 <shoma20040415@icloud.com>
2025-03-15 20:01:09 +0900

    feat: room/repositoryのinterfaceを追加

--- app/domain/room/repository.go
+++ app/domain/room/repository.go
@@ -2,6 +2,8 @@ package room
 
 type RoomRepository interface {
 	CreateRoom() (*Room, error)
+	StartRoom(roomId RoomID) (*Room, error)
+	FinishRoom(roomId RoomID) (*Room, error)
 	GetRoom(roomId RoomID) (*Room, error)
 	GetIsStart(roomId RoomID) (*Room, error)
 	GetIsFinish(roomId RoomID) (*Room, error)
