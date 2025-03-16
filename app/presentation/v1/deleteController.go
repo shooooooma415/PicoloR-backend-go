61f07cec2430a0acd5c0f27300b5b33984f2e114
shooooooma415 <shoma20040415@icloud.com>
2025-03-16 18:10:56 +0900

    feat: deleteControllerの実装

---
+++ app/presentation/v1/deleteController.go
@@ -0,0 +1,56 @@
+package v1
+
+import (
+	"encoding/json"
+	"net/http"
+	authApp "picolor-backend/app/application/auth"
+	auth "picolor-backend/app/domain/auth"
+	"picolor-backend/app/infrastructure/postgresql/utils"
+)
+
+type DeleteControllerParams struct {
+	RoomID auth.RoomID `json:"roomID"`
+	UserID auth.UserID `json:"userID"`
+}
+
+type DeleteControllerResponse struct {
+	UserID auth.UserID `json:"userID"`
+}
+
+type DeleteController struct {
+	service *authApp.AuthServiceImpl
+}
+
+func NewDeleteController(service *authApp.AuthServiceImpl) *PostController {
+	return &PostController{service: service}
+}
+
+func (pc *DeleteController) DeleteControllerHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req DeleteControllerParams
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Invalid request payload", http.StatusBadRequest)
+			return
+		}
+
+		deleteInfo := auth.DeleteUser{
+			RoomID:   req.RoomID,
+			UserID: req.UserID,
+		}
+
+		userID, err := pc.service.DeleteMember(deleteInfo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(utils.NewErrorResponse(err.Error()))
+			return
+		}
+
+		res := DeleteControllerResponse{
+			UserID: *userID,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(res)
+	}
+}
