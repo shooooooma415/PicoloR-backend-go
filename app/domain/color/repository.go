6fa9e7e9ff01d5eecdacfcde1d801a3ba237f464
shooooooma415 <shoma20040415@icloud.com>
2025-03-16 19:19:02 +0900

    feat: DeleteThemeColorsの実装

--- app/domain/color/repository.go
+++ app/domain/color/repository.go
@@ -5,5 +5,5 @@ import "picolor-backend/app/domain/auth"
 type ColorRepository interface {
 	CreateColors(CreateColor CreateColor) (*CreateColor, error)
 	GetColors(roomId auth.RoomID) ([]Color, error)
-	DeleteColors(roomId auth.RoomID) (*auth.RoomID, error)
+	DeleteThemeColors(roomId auth.RoomID) (*auth.RoomID, error)
 }
--- app/infrastructure/postgresql/repository/color.go
+++ app/infrastructure/postgresql/repository/color.go
@@ -1,1 +1,37 @@
 package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"picolor-backend/app/domain/auth"
+)
+
+type ColorRepositoryImpl struct {
+	db *sql.DB
+}
+
+func NewColorRepository(db *sql.DB) *ColorRepositoryImpl {
+	return &ColorRepositoryImpl{db: db}
+}
+
+func (q *ColorRepositoryImpl) DeleteThemeColors(roomID auth.RoomID) (*auth.RoomID, error) {
+	query := `
+		DELETE FROM room_colors
+		WHERE room_id = $1
+		RETURNING room_id
+		`
+
+	var deletedRoomID auth.RoomID
+
+	err := q.db.QueryRow(
+		query,
+		roomID,
+	).Scan(
+		&deletedRoomID,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete theme colors:%w", err)
+	}
+	return &deletedRoomID, nil
+}
