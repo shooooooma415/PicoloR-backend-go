c1e078414f6cfc849551176e9d11d512fab3b7ca
shooooooma415 <shoma20040415@icloud.com>
2025-03-16 11:06:12 +0900

    feat: createRoomMemberの実装

--- app/domain/room/room.go
+++ app/domain/room/room.go
@@ -5,13 +5,13 @@ import "time"
 type RoomID int
 
 type Room struct {
-	RoomId RoomID
+	RoomID RoomID
 	IsStart bool
 	IsFinish bool
 	StartedAT time.Time
 }
 
 type RoomMember struct {
-	RoomId RoomID
-	UserId int
+	RoomID RoomID
+	UserID int
 }
--- app/infrastructure/postgresql/repository/room.go
+++ app/infrastructure/postgresql/repository/room.go
@@ -33,3 +33,27 @@ func (q *RoomRepositoryImpl) CreateRooms() (*room.Room, error) {
 	}
 	return &createdRoom, nil
 }
+
+func (q *RoomRepositoryImpl) CreateRoomMember(user room.RoomMember) (*room.RoomMember, error) {
+	query := `
+		INSERT INTO room_members (user_id, room_id)
+		VALUES $1, $2
+		RETURNING *
+		`
+
+	var createdRoomMember room.RoomMember
+
+	err := q.db.QueryRow(
+		query,
+		user.UserID,
+		user.RoomID ,
+	).Scan(
+		&createdRoomMember,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create room member:%w", err)
+	}
+	return &createdRoomMember, nil
+}
+
