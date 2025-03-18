96d4f569ee02aaf67b3c765cf40316bf08211b29
shooooooma415 <shoma20040415@icloud.com>
2025-03-18 18:14:11 +0900

    fix: ゴルーチンで書いてる処理を同期処理に変更

--- app/application/room/resetRoom.go
+++ app/application/room/resetRoom.go
@@ -4,53 +4,33 @@ import (
 	"fmt"
 	"log"
 	"picolor-backend/app/domain/auth"
-	"sync"
 )
 
 func (c *RoomServiceImpl) ResetRoom(roomID auth.RoomID) (*auth.RoomID, error) {
-	var wg sync.WaitGroup
-	wg.Add(3)
-
 	var deletedRoomID *auth.RoomID
-	var mu sync.Mutex
 
-	go func() {
-		defer wg.Done()
-		roomID, err := c.roomRepo.DeleteRoomMembersByRoomID(roomID)
-		if err != nil {
-			log.Printf("Error deleting room members by room ID: %v", err)
-			return
-		}
-		mu.Lock()
-		deletedRoomID = roomID
-		mu.Unlock()
-	}()
+	tempRoomID, err := c.roomRepo.DeleteRoomMembersByRoomID(roomID)
+	if err != nil {
+		log.Printf("Error deleting room members by room ID: %v", err)
+		return nil, err
+	}
+	roomID = *tempRoomID
+	deletedRoomID = tempRoomID
 
-	go func() {
-		defer wg.Done()
-		roomID, err := c.postRepo.DeletePostByRoomID(roomID)
-		if err != nil {
-			log.Printf("Error deleting posts by room ID: %v", err)
-			return
-		}
-		mu.Lock()
-		deletedRoomID = roomID
-		mu.Unlock()
-	}()
+	tempPostRoomID, err := c.postRepo.DeletePostByRoomID(roomID)
+	if err != nil {
+		log.Printf("Error deleting posts by room ID: %v", err)
+		return nil, err
+	}
+	roomID = *tempPostRoomID
+	deletedRoomID = tempPostRoomID
 
-	go func() {
-		defer wg.Done()
-		roomID, err := c.colorRepo.DeleteThemeColors(roomID)
-		if err != nil {
-			log.Printf("Error deleting theme colors by room ID: %v", err)
-			return
-		}
-		mu.Lock()
-		deletedRoomID = roomID
-		mu.Unlock()
-	}()
+	_, err = c.colorRepo.DeleteThemeColors(roomID)
+	if err != nil {
+		log.Printf("Error deleting theme colors by room ID: %v", err)
+		return nil, err
+	}
 
-	wg.Wait()
 	if deletedRoomID == nil {
 		return nil, fmt.Errorf("failed to reset room with ID: %v", roomID)
 	}
